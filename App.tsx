0a538d42d2565582ce4eeb9814e92cbc4a01694b
pedro10r <pedrosantos.ti.web@gmail.com>
2022-11-03 18:39:34 -0300

    add intial config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_700Bold
+} from '@expo-google-fonts/inter';
+
+import { Routes } from './src/routes';
+
+import { theme } from './src/styles/themes/default';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_700Bold
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Routes /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+
+    primary: '#21386E',
+    primary_support: '#21386E',
+
+    shape: '#F3F3F3',
+
+    success: '#45C0A3',
+    success_light: '#53E37E',
+
+    danger: '#E63950',
+  },
+  fonts: {
+    regular: 'Inter_400Regular',
+    normal: 'Inter_500Medium',
+    bold: 'Inter_700Bold'
+  },
+}
